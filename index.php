d377c578569bb4aaac9cafb408642c20a81437c7
Stefhanie Rizo <sjesze@gmail.com>
2016-12-04 20:51:34 -0600

    jquery eliminado

--- index.php
+++ index.php
@@ -31,17 +31,7 @@
 
   <!-- Jquery -->
   <script type="text/javascript" src="/theme/js/jquery-2.1.0.min.js"></script>
-  <script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-34139801-1']);
-  _gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-  </script>
 </head>
 <body>
   <div id="loader">
