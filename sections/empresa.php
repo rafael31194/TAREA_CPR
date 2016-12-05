d4a4af5d08d3bc96d3074a8fad999e8fe784352a
Stefhanie Rizo <sjesze@gmail.com>
2016-12-04 20:57:29 -0600

    commillas raras

--- sections/empresa.php
+++ sections/empresa.php
@@ -8,13 +8,13 @@
       Realizamos soluciones tecnológicas personalizadas de acuerdo a las necesidades del cliente.
       ITSolutions cuenta con un equipo de trabajo compuesto por profesionales altamente capacitados y experimentados que le proporcionaran una optima solucion para el desarrollo de su proyecto.
     </p>
-    </div>
+  </div><br>
       <div class="title">
         <h2>Misión</h2>
       </div>
     <div class="col-md-10 col-center" style="font-size:19px;">
       <p>Ser una empresa comprometida con el desarrollo empresarial desde la Micro a la gran Empresa en El Salvador y el mundo proveyendo soluciones integrales que apoyen las actividades diarias, mejorando procesos y aumentando la calidad y eficiencia de las mismas.</p>
-    </div>
+    </div><br>
     <div class="title">
       <h2>Visión</h2>
     </div>
