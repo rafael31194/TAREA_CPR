c03ea01f45d360b4cb73bc48953823e76ddcf5d8
Stefhanie Rizo <sjesze@gmail.com>
2016-12-04 19:40:21 -0600

    cambio img slider

--- sections/carousel.php
+++ sections/carousel.php
@@ -11,7 +11,8 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
 
-    <div class="item active" style="background-image: url(http://localhost/TAREA_CPR/uploads/img/slider/carousel-item-1.jpg);">				<div class="container">
+    <div class="item active" style="background-image: url(http://localhost/TAREA_CPR/uploads/img/slider/item_1.jpg); background-size: 1220px 812px">
+      <div class="container">
       <div class="row">
         <div class="col-md-6 col-md-offset-3">
           <div style="background-color:rgba(0,0,0,0.7);padding: 18px;border-radius: 14px;">
@@ -23,7 +24,8 @@
       </div>
     </div>
   </div>
-  <div class="item" style="background-image: url(http://localhost/TAREA_CPR/uploads/img/slider/carousel-item-31.jpg);">				<div class="container">
+  <div class="item" style="background-image: url(http://localhost/TAREA_CPR/uploads/img/slider/item2.jpg);  background-size: 1220px 812px">
+    <div class="container">
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <div style="background-color:rgba(0,0,0,0.7);padding: 18px;border-radius: 14px;">
@@ -35,7 +37,8 @@
     </div>
   </div>
 </div>
-<div class="item" style="background-image: url(http://localhost/TAREA_CPR/uploads/img/slider/carousel-item-2.jpg);">				<div class="container">
+<div class="item" style="background-image: url(http://localhost/TAREA_CPR/uploads/img/slider/image10.png);  background-size: 1800px 612px">				
+  <div class="container">
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
       <div style="background-color:rgba(0,0,0,0.7);padding: 18px;border-radius: 14px;">
