ebd59f2c4420e7d8683af1502a52ed2d6318a6fc
Stefhanie Rizo <sjesze@gmail.com>
2016-12-04 22:14:38 -0600

    restaurado

--- sections/clientes.php
+++ sections/clientes.php
@@ -3,12 +3,14 @@
     <h1>Clientes<i>.</i></h1>
     <h2>Que confían en nosotros</h2>
   </div>
+
   <div class="row tiles">
+
     <div class="col-sm-6 col-lg-3">
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/0151.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/0151.jpg" alt="">
         </div>
         <div class="name">Universidad Católica de Salta Delegación Neuquén</div>
         <p class="whoiam"></p>
@@ -18,7 +20,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy" data-src="/uploads/img/clientes/logo-ifes-grupo-educativo1.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/logo-ifes-grupo-educativo1.jpg" alt="">
         </div>
         <div class="name">Grupo IFES</div>
         <p class="whoiam"></p>
@@ -28,7 +30,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/001.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/001.jpg" alt="">
         </div>
         <div class="name">Aqualic</div>
         <p class="whoiam"></p>
@@ -38,7 +40,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/016.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/016.jpg" alt="">
         </div>
         <div class="name">Sixtina Consulting Group</div>
         <p class="whoiam"></p>
@@ -48,7 +50,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/005.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/005.jpg" alt="">
         </div>
         <div class="name">Neunet</div>
         <p class="whoiam"></p>
@@ -58,7 +60,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/003.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/003.jpg" alt="">
         </div>
         <div class="name">OPS</div>
         <p class="whoiam"></p>
@@ -68,7 +70,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/007.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/007.jpg" alt="">
         </div>
         <div class="name">Remeras 1000</div>
         <p class="whoiam"></p>
@@ -78,7 +80,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/009.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/009.jpg" alt="">
         </div>
         <div class="name">Larsa</div>
         <p class="whoiam"></p>
@@ -88,7 +90,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/treo.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/treo.jpg" alt="">
         </div>
         <div class="name">TREO Cuotas diarias</div>
         <p class="whoiam"></p>
@@ -98,7 +100,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/logo-davitel.png" alt="alt-text">
+          <img src="/uploads/img/clientes/logo-davitel.png" alt="">
         </div>
         <div class="name">DA.VI.TEL. SA</div>
         <p class="whoiam"></p>
@@ -108,7 +110,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/2000px-general_electric_logo.svg_2.png" alt="alt-text">
+          <img src="/uploads/img/clientes/2000px-general_electric_logo.svg_2.png" alt="">
         </div>
         <div class="name">LUFKIN Argentina </div>
         <p class="whoiam"></p>
@@ -118,7 +120,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/kranex.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/kranex.jpg" alt="">
         </div>
         <div class="name">KRANEX Digital</div>
         <p class="whoiam"></p>
@@ -128,7 +130,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/petromark1.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/petromark1.jpg" alt="">
         </div>
         <div class="name">Petromark</div>
         <p class="whoiam"></p>
@@ -138,7 +140,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/011.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/011.jpg" alt="">
         </div>
         <div class="name">LaMar Propiedades</div>
         <p class="whoiam"></p>
@@ -148,7 +150,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/ctc.png" alt="alt-text">
+          <img src="/uploads/img/clientes/ctc.png" alt="">
         </div>
         <div class="name">Cooperativa Telefónica Centenario</div>
         <p class="whoiam"></p>
@@ -158,7 +160,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/012.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/012.jpg" alt="">
         </div>
         <div class="name">Icono</div>
         <p class="whoiam"></p>
@@ -168,7 +170,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/013.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/013.jpg" alt="">
         </div>
         <div class="name">Cuerpo y Alma</div>
         <p class="whoiam"></p>
@@ -178,7 +180,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/019.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/019.jpg" alt="">
         </div>
         <div class="name">Transac Rent a Car</div>
         <p class="whoiam"></p>
@@ -188,7 +190,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/018.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/018.jpg" alt="">
         </div>
         <div class="name">RB Consultora Financiera</div>
         <p class="whoiam"></p>
@@ -198,7 +200,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/014.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/014.jpg" alt="">
         </div>
         <div class="name">Mutual Participar</div>
         <p class="whoiam"></p>
@@ -208,7 +210,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/nrc1.jpg" alt="alt-text">
+          <img src="/uploads/img/clientes/nrc1.jpg" alt="">
         </div>
         <div class="name">Neuquén Rugby Club</div>
         <p class="whoiam"></p>
@@ -218,7 +220,7 @@
       <div class="hexagon-profile">
         <div class="hexagon-mask">
           <span class="mask"></span>
-          <img class="b-lazy"  data-src="/uploads/img/clientes/casinomagic.png" alt="alt-text">
+          <img src="/uploads/img/clientes/casinomagic.png" alt="">
         </div>
         <div class="name">Casino Magic Hotel & Casino</div>
         <p class="whoiam"></p>
