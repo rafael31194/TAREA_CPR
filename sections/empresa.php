bcef911a87880a2c49be3d429257f910b460719f
Stefhanie Rizo <sjesze@gmail.com>
2016-12-04 20:53:25 -0600

    commillas raras

--- sections/empresa.php
+++ sections/empresa.php
@@ -4,9 +4,9 @@
     <h2>Quienes Somos</h2>
   </div>
   <div class="col-md-10 col-center" style="font-size:19px;">
-    <p>“ ITSolutions es una empresa de consultoría y desarrollo de software, dedicada a brindar soluciones informáticas y a ofrecer servicios especializados en la implementación de sistemas.
+    <p>ITSolutions es una empresa de consultoría y desarrollo de software, dedicada a brindar soluciones informáticas y a ofrecer servicios especializados en la implementación de sistemas.
       Realizamos soluciones tecnológicas personalizadas de acuerdo a las necesidades del cliente.
-      ITSolutions cuenta con un equipo de trabajo compuesto por profesionales altamente capacitados y experimentados que le proporcionaran una optima solucion para el desarrollo de su proyecto. “
+      ITSolutions cuenta con un equipo de trabajo compuesto por profesionales altamente capacitados y experimentados que le proporcionaran una optima solucion para el desarrollo de su proyecto.
     </p>
     </div>
       <div class="title">
@@ -19,6 +19,6 @@
       <h2>Visión</h2>
     </div>
   <div class="col-md-10 col-center" style="font-size:19px;">
-    <p>“ Ser reconocidos como un grupo de profesionales de calidad que contribuye con la creación y puesta en marcha de proyectos empresariales de forma totalmente innovadora y que pone al servicio de las  Empresas toda su experiencia en gestión y desarrollo para el crecimiento empresarial.</p>
+    <p>Ser reconocidos como un grupo de profesionales de calidad que contribuye con la creación y puesta en marcha de proyectos empresariales de forma totalmente innovadora y que pone al servicio de las  Empresas toda su experiencia en gestión y desarrollo para el crecimiento empresarial.</p>
   </div>
   </div>
